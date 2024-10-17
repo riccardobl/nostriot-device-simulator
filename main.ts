3b895fcdf6def1fb1bc705fb798173a90800cfe3
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2024-10-17 17:09:59 +0100

    Basic nostr relay interaction happening

--- main.ts
+++ main.ts
@@ -1,5 +1,37 @@
-import { generateSecretKey, getPublicKey } from "npm:nostr-tools";
+import { finalizeEvent, getPublicKey, SimplePool } from "npm:nostr-tools";
+import { hexToBytes } from "npm:@noble/hashes/utils";
+import { loadConfigFile } from "./config.ts";
 
-const relays = [
-  "wss://relay.nostriot.com",
-];
+const appConfig = await loadConfigFile("./config.json");
+
+const sk = hexToBytes(appConfig.privateKey);
+const pk = getPublicKey(sk);
+
+let event = finalizeEvent({
+  kind: 1,
+  created_at: Math.floor(Date.now() / 1000),
+  tags: [],
+  content: "hello from deno!",
+}, sk);
+
+const pool = new SimplePool();
+
+// await Promise.any(pool.publish(appConfig.relays, event))
+// console.log('published to at least one relay!')
+
+let h = pool.subscribeMany(
+  appConfig.relays,
+  [
+    {
+      "#p": [pk],
+    },
+  ],
+  {
+    onevent(event) {
+      console.log(event);
+    },
+    oneose() {
+      h.close();
+    },
+  },
+);
