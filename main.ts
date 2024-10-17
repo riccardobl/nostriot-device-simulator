df99ab590ee0eaa59b00d267c96b8773bb3a715a
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2024-10-17 16:50:32 +0100

    deno working with nostr-tools! :)

--- main.ts
+++ main.ts
@@ -1,29 +1,7 @@
-import { AppConfig } from "./types.ts";
-import { loadConfigFile } from "./config.ts";
-import { loadPlugins } from "./plugins.ts";
+import { generateSecretKey, getPublicKey } from "npm:nostr-tools";
 
-(async () => {
-  const config = await loadConfigFile("./config.json") as AppConfig;
-  const plugins = await loadPlugins(config);
+const secretKey = generateSecretKey();
+const publicKey = getPublicKey(secretKey);
 
-  for (const plugin of plugins) {
-    for (const capability of plugin.getCapabilities()) {
-      console.log(
-        `Plugin ${plugin.constructor.name} has capability: ${capability}`,
-      );
-      switch (capability) {
-        case "runMotor": {
-          const motorParams = ["value", "2", "unit", "seconds"];
-          console.log(
-            `Executing plugin capability: ${plugin.execute(motorParams)}`,
-          );
-          break;
-        }
-        case "getTemperature": {
-          console.log(`Executing capability: ${plugin.execute()}`);
-          break;
-        }
-      }
-    }
-  }
-})();
+console.log(secretKey);
+console.log(publicKey);
